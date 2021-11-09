1042023e027ae723bcfc08bf352ee71cf954014a
Velorexe <david.bruschke@gmail.com>
2021-11-09 08:58:22 +0100

    Created basic OnMessageReceived Adapter

---
+++ Assets/Scripts/BLE/BleAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Android.BLE
+{
+    public class BleAdapter : MonoBehaviour
+    {
+        public event EventHandler<BleObject> OnMessageReceived;
+        public event EventHandler<string> OnErrorReceived;
+
+        public void OnBleMessage(string jsonMessage)
+        {
+            BleObject obj = JsonUtility.FromJson<BleObject>(jsonMessage);
+            if (obj.HasError)
+                OnErrorReceived?.Invoke(this, obj.ErrorMessage);
+            else
+                OnMessageReceived?.Invoke(this, obj);
+        }
+    }
+}
---
+++ Assets/Scripts/BLE/BleObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+namespace Android.BLE
+{
+    [Serializable]
+    public class BleObject
+    {
+        public bool HasError { get => hasError; }
+        [SerializeField]
+        private bool hasError = false;
+        
+        public string ErrorMessage { get => errorMessage; }
+        [SerializeField]
+        private string errorMessage = string.Empty;
+
+        public object ConvertMessage<T>()
+        {
+            return null;
+        }
+
+        public override string ToString() => JsonUtility.ToJson(this, true);
+    }
+}
