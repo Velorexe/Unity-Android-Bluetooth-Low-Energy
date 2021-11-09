37dc3794a222874abfcf1327a98a1319f320743b
Velorexe <david.bruschke@gmail.com>
2021-11-09 12:03:21 +0100

    Removed Singleton Pattern and added BleManager

---
+++ Assets/Scripts/BLE/BleManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Android.BLE
+{
+    public class BleManager : MonoBehaviour
+    {
+        public static BleManager Instance;
+
+        [SerializeField]
+        private BleAdapter _adapter;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            if(_adapter == null)
+            {
+                _adapter = FindObjectOfType<BleAdapter>();
+                if(_adapter == null)
+                {
+                    GameObject bleAdapter = new GameObject(typeof(BleAdapter).Name);
+                    bleAdapter.transform.SetParent(this.transform);
+
+                    _adapter = bleAdapter.AddComponent<BleAdapter>();
+                }
+            }
+        }
+    }
+}
