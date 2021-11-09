b3c40f7991de711c7a12de997a8fe00026a7165a
Velorexe <david.bruschke@gmail.com>
2021-11-09 11:51:21 +0100

    Added abstract Singleton pattern

---
+++ Assets/Scripts/BLE/Patterns/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+namespace Assets.Scripts.BLE.Patterns
+{
+    //https://blog.mzikmund.com/2019/01/a-modern-singleton-in-unity/
+    internal abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance => LazyInstance.Value;
+
+        private static readonly Lazy<T> LazyInstance = new Lazy<T>(CreateSingleton);
+
+        private static T CreateSingleton()
+        {
+            var ownerObject = new GameObject($"{typeof(T).Name} (singleton)");
+            var instance = ownerObject.AddComponent<T>();
+            DontDestroyOnLoad(ownerObject);
+            return instance;
+        }
+    }
+}
