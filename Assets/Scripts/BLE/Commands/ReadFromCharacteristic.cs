05a5a1f34be4d48ded4feb29fbb8cd9e14e4efef
Velorexe <david.bruschke@gmail.com>
2021-12-02 18:26:59 +0100

    Created Read Command

---
+++ Assets/Scripts/BLE/Commands/ReadFromCharacteristic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Android.BLE.Commands
+{
+    public class ReadFromCharacteristic : BleCommand
+    {
+        public readonly string DeviceAddress;
+
+        public readonly string Service;
+        public readonly string Characteristic;
+
+        public readonly object StringData;
+
+        public readonly bool CustomGatt;
+
+        public ReadFromCharacteristic(string deviceAddress, string serviceAddress, string characteristicAddress, string data, bool customGatt = false) : base(false, false)
+        {
+            DeviceAddress = deviceAddress;
+            Service = serviceAddress;
+            Characteristic = characteristicAddress;
+
+            StringData = data;
+
+            CustomGatt = customGatt;
+
+            _timeout = 1f;
+        }
+
+        public ReadFromCharacteristic(string deviceAddress, string serviceAddress, string characteristicAddress, byte[] data, bool customGatt = false) : base(false, false)
+        {
+            DeviceAddress = deviceAddress;
+            Service = serviceAddress;
+            Characteristic = characteristicAddress;
+
+            StringData = data;
+
+            CustomGatt = customGatt;
+
+            _timeout = 1f;
+        }
+
+        public override void Start()
+        {
+            string command = CustomGatt ? "writeToCustomGattCharacteristic" : "writeToGattCharacteristic";
+            BleManager.SendCommand(command, DeviceAddress, Service, Characteristic, StringData);
+        }
+    }
+}
