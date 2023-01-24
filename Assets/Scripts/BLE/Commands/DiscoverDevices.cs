28ecb7c77e4ca4eef00d878b2d2b6829596f1659
Velorexe <david.bruschke@gmail.com>
2023-01-24 09:33:23 +0100

    Added scanServices capability to DiscoverDevices

--- Assets/Scripts/BLE/Commands/DiscoverDevices.cs
+++ Assets/Scripts/BLE/Commands/DiscoverDevices.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static Android.BLE.Commands.ConnectToDevice;
 
 namespace Android.BLE.Commands
 {
@@ -7,8 +8,13 @@ namespace Android.BLE.Commands
         private const int StandardDiscoverTime = 10000;
 
         public readonly DeviceDiscovered OnDeviceDiscovered;
-        private int _discoverTime;
+        public readonly ServiceDiscovered OnServiceDiscovered;
 
+        private readonly int _discoverTime;
+
+        private readonly bool _scanServices = false;
+
+        #region Constructors
         public DiscoverDevices(int discoverTime = StandardDiscoverTime) : base(true, false)
         {
             _discoverTime = discoverTime;
@@ -17,17 +23,39 @@ namespace Android.BLE.Commands
         public DiscoverDevices(Action<string, string> onDeviceDiscovered, int discoverTime = StandardDiscoverTime) : base(true, false)
         {
             OnDeviceDiscovered += new DeviceDiscovered(onDeviceDiscovered);
+
             _discoverTime = discoverTime;
+            _scanServices = false;
         }
 
-        public override void Start() => BleManager.SendCommand("scanBleDevices", _discoverTime);
+        public DiscoverDevices(
+            Action<string, string> onDeviceDiscovered,
+            Action<string, string> onServiceDiscovered,
+            int discoverTime = StandardDiscoverTime) : base(true, false)
+        {
+            OnDeviceDiscovered += new DeviceDiscovered(onDeviceDiscovered);
+            OnServiceDiscovered += new ServiceDiscovered(onServiceDiscovered);
+
+            _discoverTime = discoverTime;
+            _scanServices = true;
+        }
+        #endregion
+
+        public override void Start() => BleManager.SendCommand("scanBleDevices", _discoverTime, _scanServices);
 
         public override void End() => BleManager.SendCommand("stopScanBleDevices");
 
         public override bool CommandReceived(BleObject obj)
         {
-            if (string.Equals(obj.Command, "DiscoveredDevice"))
-                OnDeviceDiscovered?.Invoke(obj.Device, obj.Name);
+            switch (obj.Command)
+            {
+                case "DiscoveredDevice":
+                    OnDeviceDiscovered?.Invoke(obj.Device, obj.Name);
+                    break;
+                case "DiscoveredDeviceService":
+                    OnServiceDiscovered?.Invoke(obj.Device, obj.Service);
+                    break;
+            }
 
             return string.Equals(obj.Command, "FinishedDiscovering");
         }
