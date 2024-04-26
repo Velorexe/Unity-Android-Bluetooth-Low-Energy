8814cdb9da293b09bce2c30b3a06c97e6fdd6d10
Paul Hayes <paul.hayes@entropedia.co.uk>
2024-04-26 17:05:26 +0100

    Adding AppPermissions class for runtime permission requests in Android 12 and above

---
+++ Runtime/BLE/AppPermissions.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.Events;
+namespace Android.BLE
+{
+
+    public class AppPermissions : MonoBehaviour
+    {
+        [System.Serializable]
+        public class PermissionData
+        {
+            public string name;
+            public bool exitOnUserDenied;
+            [NonSerialized] public bool userAuthorizedPermission;
+
+            [NonSerialized] public bool gotPermissionResponse;
+
+            public int minApiVersion = 31;
+        }
+
+        [SerializeField]
+        PermissionData[] m_permissions = new PermissionData[]{
+        new PermissionData(){name="android.permission.BLUETOOTH_SCAN",minApiVersion=31,exitOnUserDenied=true,userAuthorizedPermission=false},
+        new PermissionData(){name="android.permission.BLUETOOTH_CONNECT",minApiVersion=31,exitOnUserDenied=true,userAuthorizedPermission=false}
+    };
+
+        public UnityEvent allPermissionsGrantedEvent;
+        public UnityEvent<string> permissionDeniedEvent;
+        public UnityEvent somePermissionsDeniedEvent;
+
+        public static AppPermissions Instance
+        {
+            private set;
+            get;
+        }
+
+        [SerializeField] bool _checkPermissionsOnStart;
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            if (_checkPermissionsOnStart)
+            {
+                CheckPermissions();
+            }
+
+        }
+
+        public void CheckPermissions()
+        {
+            StartCoroutine(CheckPermissionsCoroutine());
+        }
+
+        IEnumerator CheckPermissionsCoroutine()
+        {
+            Dictionary<string, PermissionData> permissions = new Dictionary<string, PermissionData>();
+            int apiVersion = getAPIVersion();
+            //Debug.Log($"getAPIVersion={getAPIVersion()}");
+
+            foreach (var permission in m_permissions)
+            {
+                if (apiVersion < permission.minApiVersion)
+                {
+                    continue;
+                }
+                if (permission.userAuthorizedPermission = Permission.HasUserAuthorizedPermission(name))
+                {
+                    continue;
+                }
+                permissions[permission.name] = permission;
+
+            }
+            var Callbacks = new PermissionCallbacks();
+            Callbacks.PermissionDenied += (string permissionName) =>
+            {
+                Debug.LogWarning($"Permission {permissionName} denied");
+                permissions[permissionName].gotPermissionResponse = true;
+                if (permissions[permissionName].exitOnUserDenied)
+                {
+                    Application.Quit();
+                }
+            };
+            Callbacks.PermissionDeniedAndDontAskAgain += (string permissionName) =>
+            {
+                Debug.LogWarning($"Permission {permissionName} denied and don't ask again");
+                permissions[permissionName].gotPermissionResponse = true;
+                if (permissions[permissionName].exitOnUserDenied)
+                {
+                    Application.Quit();
+                }
+            };
+            Callbacks.PermissionGranted += (string permissionName) =>
+            {
+                Debug.LogWarning($"Permission {permissionName} granted");
+                permissions[permissionName].gotPermissionResponse = true;
+                permissions[permissionName].userAuthorizedPermission = true;
+            };
+
+
+            // Debug.Log($"Requesting permissions");
+            // foreach(var permission in permissions.Keys){
+            //      Debug.Log($"{permission}");
+            // } 
+
+            Permission.RequestUserPermissions(permissions.Keys.ToArray(), Callbacks);
+
+            yield return new WaitUntil(() =>
+            {
+                return permissions.All((pair) => pair.Value.gotPermissionResponse);
+            });
+
+            if (permissions.All(pair => pair.Value.userAuthorizedPermission))
+            {
+                Debug.Log("Permissions granted");
+
+                allPermissionsGrantedEvent.Invoke();
+            }
+            else
+            {
+                foreach (var permission in permissions)
+                {
+                    if (permission.Value.userAuthorizedPermission)
+                    {
+                        continue;
+                    }
+                    permissionDeniedEvent.Invoke(permission.Key);
+
+                }
+                somePermissionsDeniedEvent.Invoke();
+            }
+
+        }
+
+        static int getAPIVersion()
+        {
+            using (var version = new AndroidJavaClass("android.os.Build$VERSION"))
+            {
+                return version.GetStatic<int>("SDK_INT");
+            }
+        }
+    }
+
+}
